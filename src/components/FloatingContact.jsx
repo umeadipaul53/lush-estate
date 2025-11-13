935a2c5ff2d090db3d66a8c673aafc5dabd43721
umeadipaul53 <umeadipaul53@gmail.com>
2025-11-13 18:15:59 +0100

    added tiktok link

--- src/components/FloatingContact.jsx
+++ src/components/FloatingContact.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  FaWhatsapp,
-  FaPhoneAlt,
-  FaInstagram,
-  FaFacebookF,
-  FaYoutube,
-} from "react-icons/fa";
+import { FaInstagram, FaFacebookF, FaYoutube, FaTiktok } from "react-icons/fa";
 
 const FloatingContact = () => {
   return (
@@ -13,7 +7,7 @@ const FloatingContact = () => {
       {/** Each button is smaller and circular */}
 
       <a
-        href="https://www.instagram.com/yourprofile"
+        href="https://www.instagram.com/mrtehlex?igsh=b2pkYnh3Yjg4bnU2"
         target="_blank"
         rel="noopener noreferrer"
         className="bg-pink-500 hover:bg-pink-600 text-white p-3 rounded-full shadow-md transition transform hover:scale-110"
@@ -23,17 +17,17 @@ const FloatingContact = () => {
       </a>
 
       <a
-        href="https://www.facebook.com/yourpage"
+        href="https://www.tiktok.com/@mrtehlex?_r=1&_t=ZS-91IHxiPA2Om"
         target="_blank"
         rel="noopener noreferrer"
         className="bg-blue-600 hover:bg-blue-700 text-white p-3 rounded-full shadow-md transition transform hover:scale-110"
         title="Follow on Facebook"
       >
-        <FaFacebookF size={20} />
+        <FaTiktok size={20} />
       </a>
 
       <a
-        href="https://www.youtube.com/yourchannel"
+        href="https://youtube.com/@mrtehlex?si=uBCYrh31868yRbJT"
         target="_blank"
         rel="noopener noreferrer"
         className="bg-red-600 hover:bg-red-700 text-white p-3 rounded-full shadow-md transition transform hover:scale-110"
